db9bd3c1086abbe2a310dbc2f1b023f79a11a234
Kndarp Patel <kndarppatel@gmail.com>
2019-02-11 21:28:47 +0000

    Create post schema

---
+++ src/server/models/Post.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    postId: {
+        type: Schema.Types.ObjectId
+    },
+    postTitle: {
+        type: String,
+        required: true
+    },
+    postPermaLink: {
+        type: String,
+        required: true
+    },
+    postTimestamp: {
+        type: Date,
+        required: true
+    },
+    postAuthor: {
+        type: Schema.Types.ObjectId,
+        refs: 'users',
+        required: true
+    },
+    postImage: {
+        type: String
+    },
+    postVideo: {
+        type: String
+    },
+    postContent: {
+        type: String,
+        required: true
+    },
+    tags: [{
+        type: String
+    }],
+    share: {
+        facebook:{
+            type: String
+        },
+        twitter:{
+            type: String
+        }
+    },
+    comments: [{
+    id: {
+        type: Schema.Types.ObjectId,
+    },
+    author:{
+        type: Schema.Types.ObjectId,
+        refs: 'users'
+    },
+    timestamp:{
+        type: Date
+    },
+    content: {
+        type: String
+    },
+    score: {
+        type: Number
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+  }]
+})
+
+module.exports = Post = mongoose.model('post', PostSchema);
